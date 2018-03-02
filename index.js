42d9bfdd58153fd7548e78706e27031c940540e6
mvsemeniuk <mvsemeniuk@gmail.com>
2018-03-02 23:25:41 +0200

    style form

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
+var data = {
+    sex: null,
+    action: null,
+    height: null,
+    weight: null
+};
+
 function tryToCall() {
     var url ="https://c5541kbw26.execute-api.us-east-2.amazonaws.com/prod/";
     var xhr = new XMLHttpRequest();
     xhr.open("POST", url, true);
     xhr.onreadystatechange = function () {//Вызывает функцию при смене состояния.
         if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
-            console.log(xhr.result);
+            console.log(xhr.response);
         }
     };
 
 
     xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xhr.send(JSON.stringify({form: "232"}));
+}
+
+function selectSex(param) {
+    data.sex = param.value;
+    $("#dropdownSex").text(param.title);
+}
+
+function selectAction(param) {
+    data.action = param.value;
+    $("#dropdownAction").text(param.title);
 }
