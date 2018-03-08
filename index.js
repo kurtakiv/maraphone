2e3ad589ea6390a9b407bb5af4f39cbed8e0ba3c
mvsemeniuk <mvsemeniuk@gmail.com>
2018-03-08 13:16:45 +0200

    Auth

--- index.js
+++ index.js
@@ -6,16 +6,26 @@ var data = {
     wrist: null,
     age: null
 };
-
+var url = "https://c5541kbw26.execute-api.us-east-2.amazonaws.com/prod/";
 function submit() {
     $("#error-message2").addClass("hide-table");
     if (!validate()) return;
-    var url ="https://c5541kbw26.execute-api.us-east-2.amazonaws.com/prod/";
+
     var xhr = new XMLHttpRequest();
     xhr.open("POST", url, true);
     xhr.onreadystatechange = function () {//Вызывает функцию при смене состояния.
         if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
-           var result = JSON.parse(xhr.response).result;
+            var response = JSON.parse(xhr.response);
+            console.log(response);
+            if (response.errorMessage === "403" ) {
+                $("#calculation-panel").addClass("calculation-panel");
+                $("#error-message3").removeClass("hide-label");
+                $("#login-form").removeClass("hide-label");
+                return;
+            }
+
+
+           var result = response.result;
             $("#vvotd").text(result.vvo + " кКал");
             $("#snbtd").text(result.snb + " г");
             $("#sngtd").text(result.sng + " г");
@@ -31,6 +41,7 @@ function submit() {
 
 
     xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    data.pass = sessionStorage.getItem("pass");
     xhr.send(JSON.stringify(data));
 }
 
@@ -75,3 +86,41 @@ function validate() {
     }
     return true;
 }
+
+function login(pass) {
+  var el =  document.getElementById("passwordInput");
+   if (pass) el.value = pass;
+   var data = {
+     pass: el.value,
+     authRequest: true
+   };
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.onreadystatechange = function () {//Вызывает функцию при смене состояния.
+        if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+            var result = JSON.parse(xhr.response);
+            if (!result.errorMessage) {
+                $("#calculation-panel").removeClass("calculation-panel");
+                $("#error-message3").addClass("hide-label");
+                $("#login-form").addClass("hide-label");
+                sessionStorage.setItem('pass', el.value);
+            } else {
+                if (pass !== null) {
+                    $("#error-message3").removeClass("hide-label");
+                }
+            }
+        }
+        if (xhr.status !== 200) {
+            $("#error-message2").removeClass("hide-table");
+        }
+    };
+
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send(JSON.stringify(data));
+}
+
+window.onload = function (ev) {
+    var pass = sessionStorage.getItem("pass");
+    login(pass);
+};
