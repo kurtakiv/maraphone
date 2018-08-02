9570b32ccf505b90139ccf0bde7ade54011617e8
mykhailo semeniuk <mykhailo.semeniuk@edvantis.com>
2018-08-02 14:00:42 +0300

    Add event handlers

--- react-version/src/loginWindow/loginWindow.js
+++ react-version/src/loginWindow/loginWindow.js
@@ -1,24 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.css'
-const LoginWindow = () => {
+
+class LoginWindow extends Component {
+  state = {showError: false};
+  clickOnLogin = (d) => {
+
+    this.setState({showError: !this.state.showError});
+    console.log(this.state);
+  };
+
+  getErrorMessage() {
+    if (!this.state.showError) return "";
+    return (<label className="col-md-12 col-xs-12 hide-label error-label label label-danger">Не вірний пароль
+    </label>);
+  }
+
+
+render() {
   return (
-    <div id="login-form" className="col-md-offset-3 col-md-6  col-md-offset-3 col-xs-12">
+    <div className="col-md-offset-3 col-md-6  col-md-offset-3 col-xs-12">
       <div className="form-group">
         <label htmlFor="passwordInput">Password</label>
         <input type="password" className="form-control" id="passwordInput" placeholder="Password"/>
-          <button className="col-md-12 col-xs-12 btn btn-info btn-default btn-lg"
-                  style={{marginTop: '30px'}}
-                  type="button" id="buttonLogin"
-                  aria-haspopup="true" aria-expanded="true"
-            >
-            Увійти
-          </button>
-          <label id="error-message3"
-                 className="col-md-12 col-xs-12 hide-label error-label label label-danger">Не вірний пароль
-          </label>
+        <button className="col-md-12 col-xs-12 btn btn-info btn-default btn-lg"
+                style={ {marginTop: '30px'} }
+                type="button"
+                aria-haspopup="true"
+                aria-expanded="true"
+                onClick={ this.clickOnLogin }
+        >
+          Увійти
+        </button>
+        {this.getErrorMessage()}
       </div>
     </div>
-);
-};
+
+  );
+}
+}
 
 export default LoginWindow;
